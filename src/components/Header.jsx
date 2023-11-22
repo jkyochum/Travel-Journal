5a242daab4884851bea1dcb94efa55f486b9bd2b
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-22 13:53:39 -0500

    update scss filename and connect to project

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="/src/assets/world.svg" alt="World icon" />
+            <h3>my travel journal.</h3>
+        </header>
+    )
+}
