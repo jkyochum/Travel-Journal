04bfdc23fda9609a2b70db4dddc0ed919541d4d0
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-22 19:47:05 -0500

    import data and create object prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import Header from "/src/components/Header"
+import Card from "./components/Card"
+import cardData from "/src/js/data.js"
 import "./scss/main.scss"
 
 function App() {
 
+  const cards = cardData.map(card => {
+    console.log(card)
+    return <Card
+      key={card.title}
+      card={card}
+    />
+  })
+
   return (
     <>
       <Header />
+      <section className="card-list">
+        {cards}
+      </section>
     </>
   )
 }
