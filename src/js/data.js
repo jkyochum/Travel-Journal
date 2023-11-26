7d7569dafaaae5de7dc08f32ee35de63c13a8ce9
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-26 12:37:35 -0500

    update image urls

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ export default [
         startDate: "27 May, 2021",
         endDate: "8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "https://s3-alpha-sig.figma.com/img/3e4c/42db/87935fa3908dfc212c11483e45b24be0?Expires=1701648000&Signature=c6xavJqpHcl9LUBmfzREeBF-XuBvy4DIZH3LuOgLbxSJRP52OGhMI8cICFVCRXS-WnPb4I-VcQk~aYlWBLMjaA4bv6cO7E3AjGylE7~ZVHJ9gvWn3iBSOe229ab4GS-UPBYAMjT1ymUK8e8a1xSOF5ZYWH0C2NKMaRiyNXOZdfqZe~mOqucXfWpyJOkJqGfbX6EmoC05FuIvC1xvhAYRgk0XpCPAPuhAz5g3TGGuAJswkuFawga1C3cKrIhZhKuhUdS-giA-iAgh-2esnh4~N9JvYsvnLgds-3IrAa05rQU7qMRpeyTsITjiNUHnKs4MveRlafYUGWqyNhQcL-efsQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
     },
     {
         title: "Geirangerfjord",
@@ -24,6 +24,6 @@ export default [
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "https://s3-alpha-sig.figma.com/img/3dc0/b407/33e2a0d246e011c923d00b7ea3de39f0?Expires=1701648000&Signature=bsWNsF-ii-Cxl63oh3BlF6Q5c8538apaOjVqZtF3lNAnitLvuqB9bl-ou405hyUEsbFvu6E6KVwesIJD0FDnrSBfiDtrcn0qYwlx4U6JnkDVXaiL6xG6nvMvtheRU3U5P0ix~1pSdw3n9nRaGj38MPVawE-niKt8vwmiwC7ljRhlOVcC3iV1nGc0E3kxqDwNz3t~1SX7ruyPqgrZgfOr2MDqeQZNcrMYp2oL-Dn2qvobIwjm-OPHmtD~LE69eqZ84dq29WlyIv3CAAV7rUmxaImW8VPor5~0MHzdDzTLTJ0V4CFUYuNvsYf~T5GWdWpzV0YVtIotZ7wLflM9F-51aA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
     }
 ]
