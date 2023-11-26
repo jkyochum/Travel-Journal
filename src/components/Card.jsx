169d7ce8b8e74541e8a5cd51aae089eea438b09c
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-25 20:55:53 -0500

    add classnames to card elements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={props.card.imageUrl} />
+            <img src={props.card.imageUrl} className="card-image" />
             <div className="location">
                 <img src="./src/assets/location-marker.svg" alt="location marker" />
                 <h3>{props.card.location}</h3>
                 <p><a href={props.card.googleMapsUrl}>View on Google Maps</a></p>
             </div>
-            <h2>{props.card.title}</h2>
+            <h2 className="card-title">{props.card.title}</h2>
             <p className="visit-dates">{props.card.startDate} - {props.card.endDate}</p>
             <p className="description">{props.card.description}</p>
         </div>
