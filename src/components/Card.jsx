eaa612a17bd8f1721c524d849e9dc584ebfbec5d
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-26 12:53:13 -0500

    update anchor links to open in new tab

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card(props) {
             <div className="location">
                 <img src="./src/assets/location-marker.svg" alt="location marker" />
                 <h3>{props.card.location}</h3>
-                <p><a href={props.card.googleMapsUrl}>View on Google Maps</a></p>
+                <p><a href={props.card.googleMapsUrl} target="_blank">View on Google Maps</a></p>
             </div>
             <h2 className="card-title">{props.card.title}</h2>
             <p className="visit-dates">{props.card.startDate} - {props.card.endDate}</p>
